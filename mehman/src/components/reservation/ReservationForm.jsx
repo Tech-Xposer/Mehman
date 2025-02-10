466be5e3dc07b2f1a96c53f840aa70fcad0f07dc
Ashutosh Sharma <63233740+Tech-Xposer@users.noreply.github.com>
2025-02-10 09:19:38 +0530

    fix reservation form

--- mehman/src/components/reservation/ReservationForm.jsx
+++ mehman/src/components/reservation/ReservationForm.jsx
@@ -14,7 +14,7 @@ const ReservationForm = () => {
             <p className="form-text text-center">
               Demande de réservation{" "}
               <a href="tel:0698356157" className="link">
-                >01 46 63 45 58
+              &gt;01 46 63 45 58
               </a>{" "}
               ou remplissez le formulaire de commande
             </p>
